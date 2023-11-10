7521f3151886a7dd45dfb1c565e87a3a81724e4d
SteelBalls00 <predator7151753@mail.ru>
2023-11-10 16:34:22 +0900

    listview,detailview,base.html

--- Notice_board/views.py
+++ Notice_board/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
-from django.views.generic.base import View
+from django.views.generic import ListView, DetailView
 
 from .models import Announcement
 
 
 
-class AnnouncementsViev(View):
+class AnnouncementsViev(ListView):
+    model = Announcement
+    template_name = 'announcement_list.html'
+    announcements = Announcement.objects.all()
 
-    def get(self, request):
-        announcements = Announcement.objects.all()
-        return render(request, 'Notice_board/announcement_list.html', {'announcement_list': announcements})
+    # def get(self, request):
+        # return render(request, 'Notice_board/announcement_list.html', {'announcement_list': announcements})
+
+
+class AnnouncementsDetailViev(DetailView):
+    model = Announcement
+    template_name = 'Notice_board/announcement_detail.html'
+    context_object_name = 'announcement_detail'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['image_url'] = "http://127.0.0.1:8000/media/" + str(self.object.image)
+        return context
